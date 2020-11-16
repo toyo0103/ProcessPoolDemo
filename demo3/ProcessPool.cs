4edd9f5745a9f7f0032b3a8e7a1cc1abbe57d2d2
toyo0103 <toyo0103@gmail.com>
2020-11-16 11:01:08 +0800

    調整demo3

--- demo3/ProcessPool.cs
+++ demo3/ProcessPool.cs
@@ -37,7 +37,7 @@ namespace demo3
 
             while (this._queue.TryTake(out var task, _idleTimeout))
             {
-                process.StandardInput.WriteLine($"[Process Pool - {_name}] Do task : {task}");
+                process.StandardInput.WriteLine($"[{_name} - {Thread.CurrentThread.ManagedThreadId}] Do Task : {task}");
                 var response = process.StandardOutput.ReadLine();
                 Console.WriteLine(response);
             }
@@ -45,8 +45,8 @@ namespace demo3
             process.StandardInput.Close();
             process.WaitForExit();
             _threads.TryRemove(Thread.CurrentThread.ManagedThreadId, out var t);
-            Console.WriteLine($"[Process Pool - {_name}] idle timeout");
-            Console.WriteLine($"[Process Pool - {_name}] close process");
+            Console.WriteLine($"[{_name} - {Thread.CurrentThread.ManagedThreadId}] idle timeout");
+            Console.WriteLine($"[{_name} - {Thread.CurrentThread.ManagedThreadId}] close process");
         }
 
         private Process GenerateProcess()
