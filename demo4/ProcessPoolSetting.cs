90ce0ff7745c5f886ec1db33bc0ce27f2f611828
toyo0103 <toyo0103@gmail.com>
2020-11-16 11:37:41 +0800

    demo4

---
+++ demo4/ProcessPoolSetting.cs
@@ -0,0 +1,9 @@
+namespace demo4
+{
+    public class ProcessPoolSetting
+    {
+        public string Name{get;set;}
+        public int MaxProcess{get;set;}
+        public int MinProcess{get;set;}
+    }
+}
